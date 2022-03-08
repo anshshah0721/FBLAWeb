59bc41e973b7ff0ef8e574803b21aeb21e3e7cc1
hale665 <tomhale247@gmail.com>
2022-03-08 16:13:15 -0600

    fixed again

--- script.js
+++ script.js
@@ -383,7 +383,7 @@ const restaurant = {
               formattedPlace = formattedPlace + placeArray[i] + ' ';
             }
             // this formats our output and hsows the output in their repsctive places
-            let temp = "window.location.href = \'"+ window.location.href + "/?place=" + place +'\''
+            let temp = "window.location.href = \'"+ window.location.href + "?place=" + place +'\''
             temp = temp.replace("locative.html", "locationData.html")
             specText.innerHTML = specText.innerHTML + '<div class="boxes" id= \"' + place + '\" onclick = \"' + temp + '\" ><h1 class="text">' + formattedPlace + '</h1>' + '</div>';
             currentPlaces.push(place)
@@ -410,7 +410,7 @@ const restaurant = {
             for (var i = 0; i < placeArray.length; i++) {
               formattedPlace = formattedPlace + placeArray[i] + ' ';
             }
-            let temp = "window.location.href = \'"+ window.location.href + "/?place=" + place +'\''
+            let temp = "window.location.href = \'"+ window.location.href + "?place=" + place +'\''
             temp = temp.replace("locative.html", "locationData.html")
             specText.innerHTML = specText.innerHTML + '<div class="boxes" id= \"' + place + '\" onclick = \"' + temp + '\" ><h1 class="text">' + formattedPlace + '</h1>' + '</div>';
             currentPlaces.push(place)
